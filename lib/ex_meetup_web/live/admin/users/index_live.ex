32bb60f2ffda8eb70e8cad8d17375d327a009956
Saverio Trioni <saverio.trioni@kantox.com>
2019-10-31 10:25:58 +0100

    WIP

--- lib/ex_meetup_web/live/admin/users/index_live.ex
+++ lib/ex_meetup_web/live/admin/users/index_live.ex
@@ -5,8 +5,26 @@ defmodule ExMeetupWeb.Admin.Users.IndexLive do
     Phoenix.View.render(ExMeetupWeb.UserView, "index.html", assigns)
   end
 
+  def mount(_session, socket) do
+    {:ok, socket}
+  end
+
   def handle_params(params, _uri, socket) do
     users = ExMeetup.Admin.list_users()
-    {:noreply, assign(socket, users: users)}
+
+    params =
+      params
+      |> Map.put_new(:page, %{})
+      |> update_in(~w[page number], fn
+        nil -> 1
+        n -> String.to_integer(n)
+      end)
+      |> update_in(~w[page size], fn
+        nil -> 20
+        n -> String.to_integer(n)
+      end)
+
+    socket = socket |> assign(users: users, query_params: params, total_pages: 10)
+    {:noreply, socket}
   end
 end
--- lib/ex_meetup_web/views/user_view.ex
+++ lib/ex_meetup_web/views/user_view.ex
@@ -1,3 +1,25 @@
 defmodule ExMeetupWeb.UserView do
   use ExMeetupWeb, :view
+  import Plug.Conn.Query, only: [encode: 1]
+
+  def pagination(qp, total_pages) do
+    IO.inspect(qp)
+    current_page = get_in(qp, ~w[page number]) || 1
+
+    content_tag(:div, class: "pagination") do
+      1..total_pages
+      |> Enum.map(fn
+        1 -> 1
+        ^total_pages -> total_pages
+        n when n < current_page - 1 -> nil
+        n when n > current_page + 1 -> nil
+        n -> n
+      end)
+      |> Enum.dedup()
+      |> Enum.map(fn
+        nil -> content_tag(:span, "...")
+        n -> live_link("#{n}", to: "?#{qp |> put_in(~w[page number], n) |> encode()}")
+      end)
+    end
+  end
 end
